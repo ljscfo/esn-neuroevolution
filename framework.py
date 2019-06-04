dfc2956cfe58946601867e9ecd05c6ac04e1edff
rek <15801406+ljscfo@users.noreply.github.com>
2019-06-04 15:07:29 +0200

    Conversation between different network formats

--- framework.py
+++ framework.py
@@ -2,11 +2,22 @@
 import sys, os
 import random
 import numpy as np
+import math
 import matplotlib.pyplot as plt
+from collections import defaultdict
+from functools import partial
 
 sys.path.append(os.path.join(os.path.split(__file__)[0],'..','..'))
 from peas.methods.neat import NEATPopulation, NEATGenotype
 from peas.networks.rnn import NeuralNetwork
+
+
+#from pylab import *
+#import pandas as pd
+#import seaborn as sns
+
+from ESN_CELL import *
+from ddeint import ddeint
 
 
 
@@ -50,7 +61,6 @@ class ESNTask_internal(object):
             raise Exception("Network should be able to output value of -1, e.g. using a tanh node.")
 
         pairs = list(zip(self.INPUTS, self.OUTPUTS))
-        #print(pairs[0:10])
 
         for (i, target) in pairs[:1000]:
             # Feed with bias
@@ -84,40 +94,49 @@ class ESNTask_internal(object):
 
 class ESNTask_external(object):
 
-
     def __init__(self):
         pass
 
     def evaluate(self, network, verbose=False):
+        #Still needs a lot of work
+        matrices = get_weight_matrices()
 
-        #Network to ESN_CELL
-        nodes = network.node_genes   #: [] Tuples of (fforder, type, bias, response, layer)
-        conns = network.conn_genes    #: {} Tuples of (innov, from, to, weight, enabled) conn[(i, j)]
+        res_units = 100
+        in_units = 1
+        ESN_arch = [in_units, res_units]
 
-        output_layer = np.int64(0) #output layer is max value for int64, but it's checked just in case
+        leakrates = [0.1, 0.25, 0.5, 0.75, 0.9]
+        activation = np.tanh
+        weights_variance = 0.1
+        sparsity = 0.1
 
-        nodes_per_layer = defaultdict(lambda: 0)
-        print("TestDD:", nodes_per_layer[42])
+        esn = ESN(ESN_arch, activation, leakrates[0], weights_variance, sparsity, matrices)
 
-        for i, node in enumerate(nodes):
-            layer = node[4]
-            nodes_per_layer[layer] += 1
-            # Output layer has highest layer number
-            if layer > output_layer:
-                output_layer = layer
+        #TODO: don't do that step for every evaluation but once
+        model = lambda X,t,beta,n,tau,gamma : beta*((X(t-tau))/(1 + X(t-tau)**n)) - gamma*X(t)
 
-        n_res_nodes = 0 #Amount of reservoir nodes
-        for layer in nodes_per_layer:
-            if layer != 0 and layer != output_layer:
-                n_res_nodes += nodes_per_layer[layer]
+        X_0 = lambda t:0.5 # history before t=0
 
-        weights_in = np.zeros((nodes_per_layer[0],n_res_nodes))
+        X_ts = {}
 
-        for conn_key in conns:
-            conn = conns[conn_key]
-            enabled = conn[4]
-            if :
-                if conn[]
+        bifurcation_para = [10]
+
+        for i, n in enumerate(bifurcation_para):
+            t = np.linspace(0,30,3000)
+            X_t = ddeint(model, X_0, t, fargs=(2, n, 2, 1)) #beta=2, n=n, tau=2, gamma=1
+            X_ts[n] = X_t
+
+            #length of input series
+        len_in = len(X_ts[10])
+
+        esn_input = X_ts[10].reshape([in_units, len_in]) # of shape: [in_units, len_in]
+
+        print("ESN_inp_type",type(esn_input))
+        print(esn_input.shape)
+
+        esn.res_states(esn_input, np.zeros((1,res_units)))
+
+        #TODO: Continue working here
 
         graph = [list(),list()]
 
@@ -130,42 +149,83 @@ class ESNTask_external(object):
         score = self.evaluate(network)
         return score['fitness'] > 0.9
 
-def get_network(network):
-    nodes = network.node_genes   #: [] Tuples of (fforder, type, bias, response, layer)
+
+def get_weight_matrices(network):
+
+    matrix = network.get_network_data()[0]
+
+    #TODO: Check if bias is node or not (default False, but property of NEATGenotype)
+    bias = 1
+
+    nodes = network.node_genes   #: [] Tuples of (fforder, type, bias, responsraininge, layer)
     conns = network.conn_genes    #: {} Tuples of (innov, from, to, weight, enabled) conn[(i, j)]
-    print("Nodes:",nodes)
-    print("Connections:",conns)
 
-    """
-    Nodes: [[0.0, 'tanh', 0.0, 4.924273, 0], [1024.0, 'tanh', 0.0, 4.924273, 9223372036854775807], [512.0, 'tanh', 0.0, 4.924273, 1]]
-    Connections: {(0, 1): [0, 0, 1, 0.038451762473330264, False], (0, 2): [1, 0, 2, 1.0, True], (2, 1): [2, 2, 1, 0.038451762473330264, True]}
+    output_layer = np.int64(0) #output layer is max value for int64, but it's checked just in case
 
-    Nodes: [[0.0, 'tanh', 0.0, 4.924273, 0], [1024.0, 'tanh', 0.0, 4.924273, 9223372036854775807], [512.0, 'tanh', 0.9850203970958736, 4.924273, 1], [256.0, 'tanh', 0.0, 4.924273, 1], [768.0, 'tanh', 0.0, 4.924273, 2], [384.0, 'tanh', 0.0, 4.924273, 2]]
-    Connections: {(0, 1): [0, 0, 1, 0.21498898225512877, True], (0, 2): [1, 0, 2, 1.0, True], (2, 1): [2, 2, 1, 0.03234019480256184, False], (1, 2): [4, 1, 2, 0.05097810092701511, True], (3, 2): [5, 3, 2, 1.0, False], (0, 3): [7, 0, 3, 1.0, True], (4, 1): [8, 4, 1, 0.03234019480256184, True], (3, 5): [10, 3, 5, 1.0, True], (5, 2): [11, 5, 2, 1.0, True], (1, 5): [12, 1, 5, -1.526050757421022, True]}
+    nodes_per_layer = defaultdict(lambda: 0)
+    node_layer = np.zeros(len(nodes) + bias)
 
-    """
+    for i, node in enumerate(nodes):
+        layer = node[4]
+        nodes_per_layer[layer] += 1
+        # Output layer has highest layer number
+        if layer > output_layer:
+            output_layer = layer
+
+    node_layer[0] = 1
+    for i, node in enumerate(nodes):
+        layer = node[4]
+        if layer == 0:
+            node_layer[i+bias] = 0
+        elif layer == output_layer:
+            node_layer[i+bias] = 3
+        else:
+            node_layer[i+bias] = 2
+
+    weights_in = matrix[:,node_layer == 0][node_layer == 2,:]
+    weights_bias = matrix[:,node_layer == 1][1:,:]
+    weights_reservoir = matrix[:,node_layer == 2][node_layer == 2,:]
+    weights_out = matrix[:,node_layer == 2][node_layer == 3,:]
+
+    return weights_in, weights_bias, weights_reservoir, weights_out
+
+def random_topology(n_nodes, sparsity):
+    #just create tuples of kind (source_node, target_node) in a complicated way
+
+    topology = defaultdict(lambda: 0)
+
+    for i in range(math.floor((n_nodes**2-n_nodes)*sparsity)):
+        while True:
+            source = np.random.randint(0,n_nodes)
+            target = np.random.randint(1,n_nodes)
+            #TODO: Connection to self possible or not?
+            if (source != target and topology[(source,target)] == 0):
+                break
+        topology[(source,target)] = 1
+
+    return topology
 
 def init():
-    genotype = lambda: NEATGenotype(inputs=1, weight_range=(-1, 1), types=['tanh'], feedforward = False,prob_add_node=0.2,prob_add_conn=0.5)
+    topology = random_topology(10, 0.5)
+    genotype = lambda: NEATGenotype(inputs=1, outputs=1, topology = topology, types=['tanh'], feedforward = False,prob_add_node=0.2,prob_add_conn=0.5)
 
     # Create a population
     pop = NEATPopulation(genotype, popsize=50)
 
     # Create a task
     task = ESNTask_internal()
-
     return task, pop
+
 
 def run(task, population):
     for i in range(1):
         # Run the evolution, tell it to use the task as an evaluator
         pop.epoch(generations=10, evaluator=task, solution=task)
 
-    get_network(pop.champions[-1])
+    print(get_weight_matrices(pop.champions[-1]))
 
     final = task.evaluate(pop.champions[-1])
 
-    #print("Final:",final)
     plt.plot(final['graph'][0][:400],label = "Original")
     plt.plot(final['graph'][1][:400], label = "predicted")
     plt.show()
