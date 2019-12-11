e70a757580fe9a04713d91051d98d65a63edf06e
Taher Habib <33577096+TaherHabib@users.noreply.github.com>
2019-12-11 17:36:00 +0100

    Last version. Changes to Lyapunov Expo Calculation

--- esn_cell.py
+++ esn_cell.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
+import copy
+
 from sklearn.linear_model import Ridge
 
-#TODO: use consitently either shape [timeserieslength x e.g.in_units] or the other way round but not both
 class ESN():
 
-    d_0 = np.random.normal(size=[], loc=1e-12, scale=1e-13)
-
-    def __init__(self, ESN_arch, activation=np.tanh, leak_rate=0.0, weights_std=0.1, sparsity=0.1, weights_external = None, set_spectral_radius = None):
+    def __init__(self, ESN_arch, activation=np.tanh, leak_rate=1.0, weights_std=0.1, sparsity=0.1, \
+                 weights_external=None, use_normalized_weights=False):
 
         """
         Args:
@@ -18,7 +18,6 @@ class ESN():
             sparsity: float64, [0,1], sparseness of the reservoir weight matrix. Default: 0.1.
             weights_external: if None, reservoir weights are created randomly,
               otherwise, a list of length 3 or 4 is expected, entries being weight matrices for input, bias, reservoir and optionally output
-            spectral_radius: if not None: changes the weigths so the spectral radius of the reservoir to that value
         """
 
         self.ESN_arch = ESN_arch
@@ -31,10 +30,6 @@ class ESN():
         self.weights_std = np.float64(weights_std)
         self.sparsity = np.float64(sparsity)
 
-        # All instantiations of the class have the same random seed
-        # and hence the same weight initializations
-        #np.random.seed(123)
-
         if weights_external != None:
             #Weights are given and don't need to be initialized randomly
 
@@ -46,45 +41,42 @@ class ESN():
                 self.weights_out = weights_external[3]
             else:
                 self.weights_out = np.random.normal(size=[self.res_units, self.out_units], scale=self.weights_std)
+        
         else:
-            # Initialize 'W_in'
+            
             self.weights_in = np.random.normal(size=[self.in_units, self.res_units], scale=self.weights_std)
             # dims: [in_units, res_units]
 
-            # Initialize 'W_res'
             self.weights_res = np.random.normal(size=[self.res_units, self.res_units], scale=self.weights_std)
             # dims: [res_units, res_units]
 
-            # Initialize 'bias'
-            self.bias = np.random.normal(size=[1, self.res_units], scale=self.weights_std)
+            self.bias = np.random.normal(size=[1, self.res_units], scale=1e-40)
             # dims: [1, res_units]
 
             self.weights_out = np.random.normal(size=[self.res_units, self.out_units], scale=self.weights_std)
-            # Compute sparse_mask
-            # dims: [res_units, res_units]
+            # dims: [res_units, out_units]
+            
             self.sparse_mask = np.float64(np.less_equal(np.random.uniform(size=[self.res_units, self.res_units]), \
                                                         self.sparsity))
-
+            # dims: [res_units, res_units]
+            
             # W_res is transformed for making it sparse
             self.weights_res = np.multiply(self.weights_res, self.sparse_mask)
-
-
-
+        
         # Compute Spectral Radius
-        #print(np.linalg.eigvals(self.weights_res))
         self.spectral_radius = np.abs(np.linalg.eigvals(self.weights_res)).max()
-
-        if (set_spectral_radius != None):
-            # W_res is normalized wrt spectral_radius
-            self.weights_res = np.multiply(self.weights_res, set_spectral_radius/(self.spectral_radius))
-            self.spectral_radius = np.abs(np.linalg.eigvals(self.weights_res)).max()
+   
+        if (use_normalized_weights):
+            self.weights_res = np.multiply(self.weights_res, 1/(self.spectral_radius))
+            self.normalized_spectral_radius = np.abs(np.linalg.eigvals(self.weights_res)).max()
+            
 
     def res_states(self, inputs, init_state, compute_readout = False):
 
         """ Runs one feedforward step of ESN.
 
         Args:
-          inputs: shape [self.in_units x <timeserieslength>]
+          inputs: shape [<timeserieslength> x self.in_units]
           init_state: shape [1 x self.res_units]
           compute_readout: True if readout of esn should be computed
 
@@ -96,32 +88,29 @@ class ESN():
         """
         res_states = []
         outputs = []
+        
         res_state = init_state
-        len_in = len(inputs[0])
-
-
+ 
+        len_in = int(np.size(inputs)/self.in_units)
+    
         for n in range(len_in):
-            """
-            print(res_state.shape,self.weights_res.shape)
-            print(np.matmul(res_state, self.weights_res).shape)
-            print("some",self.weights_in.shape,self.bias.shape)
-            """
-            new_state = self.activation(np.multiply(self.weights_in, inputs[0][n]) + \
+            
+            new_state = self.activation(np.multiply(inputs[n] ,self.weights_in) + \
                                         np.matmul(res_state, self.weights_res) + \
                                         self.bias)
-
+            
             res_state = np.multiply(1-self.leak_rate, res_state) + np.multiply(self.leak_rate, new_state)
             res_states.append(res_state)
-
-            #TODO: Or output before leak?, different activiation function for output?, how to include activation into ridge
+            
             if compute_readout:
                 output = self.out_states(res_state)
                 outputs.append(output)
 
         res_states = np.array(res_states).reshape([len_in, self.res_units]) # dims: [<timeserieslength>, self.res_units]
-
+        
         return res_states, res_state, np.array(outputs)
 
+    
     #Computes the readout of the esn for one timestep (given the reservoir state and the readout weigths)
     def out_states(self, res_state):
         return np.matmul(res_state,self.weights_out).flatten()
@@ -138,7 +127,7 @@ class ESN():
             new weights that linarly map res_states to corresponding target values, shape [self.res_units x self.out_units]
         """
 
-        ridge = Ridge(alpha=0.000000002, fit_intercept = False)#, fit_intercept = True, normalize = True)
+        ridge = Ridge(alpha=0.0000001, fit_intercept = False)#, fit_intercept = True, normalize = True)
         #packed_targets = [(target) for target in targets]
         ridge.fit(res_states, targets)
         #print(res_states.shape,targets.shape,ridge.coef_.shape)
@@ -146,73 +135,73 @@ class ESN():
 
         return new_weights_out
 
-    def lyapunov_exponent(self, inputs, init_state):
+    
+    def lyapunov_exponent(self, inputs, init_state, perturbation_order=1e-10):
 
         """
         Computes Lyapunov Exponent of the ESN
 
         Args:
-          inputs: shape [self.in_units x <timeserieslength>]
+          inputs: shape [<timeserieslength> x self.in_units]
           init_state: shape [1 x self.res_units]
+          perturbation_order: the order of perturbation of the state of the ESN
 
         Returns:
           A tuple (lyapunov_exp, df_n_avgdist)
-
         """
 
+        pert_ord = perturbation_order
+        len_in = int(np.size(inputs)/self.in_units)
         res_states = self.res_states(inputs=inputs, init_state=init_state)[0]
-        len_in = len(inputs[0])
 
         # Instantiating a copy ESN
         copy_esn = ESN(self.ESN_arch, self.activation, self.leak_rate, self.weights_std, self.sparsity)
         copy_esn.weights_in = self.weights_in
         copy_esn.weights_res = self.weights_res
         copy_esn.bias = self.bias
-        copy_esn.spectral_radius = self.spectral_radius
-
+        
         # length of discarded initial transient
         init_transient = 999
 
-        # d_0 is the perturbation magnitude of order 1e-8
-        d_0 = self.d_0
-
-        df_n_avgdist = pd.DataFrame(columns=['Perturbed_Neuron','Initial_Perturbation','Avg_Dist'])
+        #df_n_avgdist = pd.DataFrame(columns=['Perturbed_Neuron','Initial_Perturbation','Avg_Dist'])
         ln_d1_d0 = []
-
 
         for pert_n in range(self.res_units):
 
-            dist_esns = []
-
-            # Extracting Initial State for a Copy of ESN
-            init_copy_esn = res_states[init_transient].reshape([1, self.res_units])
+            d_0 = np.random.uniform(pert_ord, 1.1*pert_ord, size=[])
+            
+            # Extracting Initial State for Copy of ESN
+            init_copy_esn = copy.deepcopy(res_states[init_transient].reshape([1, self.res_units]))
 
             #Perturbing 'pert_n'
-            np.put(init_copy_esn, ind=pert_n, v=d_0)
+            np.put(init_copy_esn, ind=pert_n, v=d_0+init_copy_esn[0][pert_n])
 
             for step in range(1, len_in-init_transient):
 
-                # output of esn
-                out_esn = res_states[init_transient+step].reshape([1, self.res_units])
+                # res_state of 'esn' after 'step' timesteps of initial transient
+                res_esn = res_states[init_transient+step].reshape([1, self.res_units])
 
-                # one timestep Input for copy_esn
-                input_copy_esn = inputs[:,init_transient+step].reshape([1,1]) # dims: [1,1]
+                # one timestep input for 'copy_esn'
+                input_copy_esn = inputs[init_transient+step] # dims: [1, in_units]
 
-                # output of copy_esn
-                out_copy_esn = copy_esn.res_states(inputs=input_copy_esn, init_state=init_copy_esn)[0] # dims: [1,100]
-
+                # res_state of 'copy_esn' after one iteration over input
+                res_copy_esn = copy_esn.res_states(inputs=input_copy_esn, init_state=init_copy_esn)[0] # dims: [1, res_units]
+                
                 # Euclidean distance between esn and copy_esn after 1 timestep
-                d_1 = np.linalg.norm(out_copy_esn-out_esn)
+                d_1 = np.linalg.norm(res_copy_esn-res_esn)
 
-                dist_esns.append(d_1)
-                ln_d1_d0.append(np.log(d_1/d_0))
-
-                # Normalizing the state of esn_2 to the distance d_0
-                # for next iteration over the next input timestep
-                init_copy_esn = out_esn + np.multiply((d_0/d_1),np.subtract(out_copy_esn, out_esn))
-
-            df_n_avgdist.loc[pert_n+1] = [pert_n+1, d_0, np.mean(dist_esns)]
+                if (d_1==0):
+                    init_copy_esn = copy.deepcopy(res_states[init_transient+step].reshape([1, self.res_units]))
+                    d_0 = np.random.uniform(pert_ord, 1.1*pert_ord, size=[])
+                    np.put(init_copy_esn, ind=pert_n, v=d_0+init_copy_esn[0][pert_n])
+                else:
+                    ln_d1_d0.append(np.log(d_1/d_0))
+                    # Normalizing state of copy_esn to distance d_0 for next iteration over next input timestep
+                    init_copy_esn = res_esn + np.multiply((d_0/d_1),np.subtract(res_copy_esn, res_esn))
+                    
+                    
+            #df_n_avgdist.loc[pert_n+1] = [pert_n+1, d_0, np.mean(dist_esns)]
 
         lyapunov_exp = np.mean(ln_d1_d0)
-
-        return lyapunov_exp#, df_n_avgdist
+        
+        return lyapunov_exp, ln_d1_d0
