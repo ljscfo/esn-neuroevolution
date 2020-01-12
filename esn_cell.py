b25e9c98c95368fcfdd64c303881be9e865920ea
rek <15801406+ljscfo@users.noreply.github.com>
2020-01-13 00:28:39 +0100

    Fixed comment

--- esn_cell.py
+++ esn_cell.py
@@ -16,7 +16,7 @@ class ESN():
             ESN_arch: 1-D int array, [no. of input units, no. of reservoir units, no. of output units] .
             activation: Nonlinear activation function.  Default: `np.tanh`.
             leak_rate: float64, (0,1], leaking rate of the reservoir units (alpha)
-            weights_std: float64, variance of the normal dist. used in initializing weight matrices.
+            weights_std: float64, standard deviation of the normal dist. used in initializing weight matrices.
             sparsity: float64, [0,1], sparseness of the reservoir weight matrix. Default: 0.1.
             weights_external: if None, reservoir weights are created randomly,
               otherwise, a list of length 3 or 4 is expected, entries being weight matrices for input, bias, reservoir and optionally output
@@ -43,9 +43,9 @@ class ESN():
                 self.weights_out = weights_external[3]
             else:
                 self.weights_out = np.random.normal(size=[self.res_units, self.out_units], scale=self.weights_std)
-        
+
         else:
-            
+
             self.weights_in = np.random.normal(size=[self.in_units, self.res_units], scale=self.weights_std)
             # dims: [in_units, res_units]
 
@@ -57,21 +57,21 @@ class ESN():
 
             self.weights_out = np.random.normal(size=[self.res_units, self.out_units], scale=self.weights_std)
             # dims: [res_units, out_units]
-            
+
             self.sparse_mask = np.float64(np.less_equal(np.random.uniform(size=[self.res_units, self.res_units]), \
                                                         self.sparsity))
             # dims: [res_units, res_units]
-            
+
             # W_res is transformed for making it sparse
             self.weights_res = np.multiply(self.weights_res, self.sparse_mask)
-        
+
         # Compute Spectral Radius
         self.spectral_radius = np.abs(np.linalg.eigvals(self.weights_res)).max()
-   
+
         if (use_normalized_weights):
             self.weights_res = np.multiply(self.weights_res, 1/(self.spectral_radius))
             self.normalized_spectral_radius = np.abs(np.linalg.eigvals(self.weights_res)).max()
-            
+
 
     def res_states(self, inputs, init_state, compute_readout = False):
 
@@ -90,29 +90,29 @@ class ESN():
         """
         res_states = []
         outputs = []
-        
+
         res_state = init_state
- 
+
         len_in = int(np.size(inputs)/self.in_units)
-    
+
         for n in range(len_in):
-            
+
             new_state = self.activation(np.multiply(inputs[n] ,self.weights_in) + \
                                         np.matmul(res_state, self.weights_res) + \
                                         self.bias)
-            
+
             res_state = np.multiply(1-self.leak_rate, res_state) + np.multiply(self.leak_rate, new_state)
             res_states.append(res_state)
-            
+
             if compute_readout:
                 output = self.out_states(res_state)
                 outputs.append(output)
 
         res_states = np.array(res_states).reshape([len_in, self.res_units]) # dims: [<timeserieslength>, self.res_units]
-        
+
         return res_states, res_state, np.array(outputs)
 
-    
+
     #Computes the readout of the esn for one timestep (given the reservoir state and the readout weigths)
     def out_states(self, res_state):
         return np.matmul(res_state,self.weights_out).flatten()
@@ -137,7 +137,7 @@ class ESN():
 
         return new_weights_out
 
-    
+
     def lyapunov_exponent(self, res_states=None, compute_res_states=False, inputs=None, init_state=None, perturbation_order=1e-12):
 
         """
@@ -154,7 +154,7 @@ class ESN():
 
         pert_ord = perturbation_order
         len_in = int(np.size(inputs)/self.in_units)
-        
+
         if compute_res_states:
             res_states = self.res_states(inputs=inputs, init_state=init_state)[0]
 
@@ -163,10 +163,10 @@ class ESN():
         copy_esn.weights_in = self.weights_in
         copy_esn.weights_res = self.weights_res
         copy_esn.bias = self.bias
-        
+
         # length of discarded initial transient
         init_transient = 999
-        
+
         # No. of samples for computing Lyapunov Exponent
         target_nof_samples = (len_in-init_transient-1)*self.res_units
 
@@ -176,7 +176,7 @@ class ESN():
         for pert_n in range(self.res_units):
 
             d_0 = np.random.uniform(pert_ord, 1.1*pert_ord, size=[])
-            
+
             # Extracting Initial State for Copy of ESN
             init_copy_esn = copy.deepcopy(res_states[init_transient].reshape([1, self.res_units]))
 
@@ -193,7 +193,7 @@ class ESN():
 
                 # res_state of 'copy_esn' after one iteration over input
                 res_copy_esn = copy_esn.res_states(inputs=input_copy_esn, init_state=init_copy_esn)[0] # dims: [1, res_units]
-                
+
                 # Euclidean distance between esn and copy_esn after 1 timestep
                 d_1 = np.linalg.norm(res_copy_esn-res_esn)
 
@@ -205,31 +205,31 @@ class ESN():
                     ln_d1_d0.append(np.log(d_1/d_0))
                     # Normalizing state of copy_esn to distance d_0 for next iteration over next input timestep
                     init_copy_esn = res_esn + np.multiply((d_0/d_1),np.subtract(res_copy_esn, res_esn))
-                    
-                    
+
+
             #df_n_avgdist.loc[pert_n+1] = [pert_n+1, d_0, np.mean(dist_esns)]
 
         LE = np.mean(ln_d1_d0)
-        
+
         percent_nof_samples = (len(ln_d1_d0)/target_nof_samples)*100
-        
+
         return LE, percent_nof_samples
-    
-    
+
+
     def transfer_entropy(self, res_states=None, compute_res_states=False, inputs=None, init_state=None):
-        
+
         """
         Computes Transfer Entropy of the ESN using 'IDTxl' library
 
         Args:
-        
+
         """
-        
+
         sources_list = []
         targets_list = []
         nonzero_connect = 0
         total_TE = 0
-        
+
         for n in range(self.res_units):
             sources = []
             for i in range(self.res_units):
@@ -237,20 +237,20 @@ class ESN():
                     nonzero_connect += 1
                     if i!=n:
                         sources.append(i)
-            
+
             if len(sources)!=0:
                 targets_list.append(n)
                 sources_list.append(sources)
-            
+
         if compute_res_states:
             res_states = self.res_states(inputs=inputs, init_state=init_state)[0]
-        
+
         # length of discarded initial transient
         init_transient = 999
-        
+
         # Dataset for IDTxl MultivariateTE()
         d = Data(res_states[init_transient:,:], dim_order='sp')
-        
+
         # Settings for MultivariateTE() computation
         settings = {'cmi_estimator': 'JidtKraskovCMI',
                     'max_lag_sources': 2,
@@ -260,15 +260,15 @@ class ESN():
                     'n_perm_omnibus': 400,
                     'n_perm_max_seq': 400,
                     'verbose':False}
-        
-        TE_results = MultivariateTE().analyse_network(settings=settings, data=d, 
+
+        TE_results = MultivariateTE().analyse_network(settings=settings, data=d,
                                                    targets=targets_list, sources=sources_list)
-        
+
         for i in targets_list:
-            
+
             if TE_results.get_single_target(i, fdr=False).omnibus_te != None:
                 total_TE += TE_results.get_single_target(i, fdr=False).omnibus_te
-            
+
         avg_TE = total_TE/len(targets_list)
-            
+
         return TE_results, avg_TE, nonzero_connect
